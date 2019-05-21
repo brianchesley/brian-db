3fea653ab21a725e0cda32c88e7080054c6b1271
Brian <chesley.brian@gmail.com>
2019-05-21 17:49:53 -0400

    lesson 2

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
 def main():
-    input_buffer = input('db > ')
+    input_buffer = input("db > ")
     while True:
-        if input_buffer == '.exit':
-            exit()
+        if input_buffer[0] == '.':
+            meta_command(input_buffer)
+    
+        statement = Statement(input_buffer)
+        if statement.valid:
+            pass
         else:
-            print("input not recognized  ", input_buffer)
-            break
+            print("Unrecognized keyword at start of '%s'.\n")
+
+    execute_statement(statement)
+    print("Executed.\n")
+
+def meta_command(input):
+    if input == ".exit":
+        exit()
+    else:
+        print('command not recognized')
+
+class Statement():
+    def __init__(self, input_buffer):
+        self.type = self.get_statement(input_buffer)
+        self.valid = False
+
+    def get_statement(self, input):
+        if input.upper == 'INSERT':
+            self.type = 'INSERT'
+            self.valid = True
+        elif input.upper() == 'SELECT':
+            self.type = 'SELECT'
+            self.valid = True
+        else:
+            self.valid = False
+
+def execute_statement(statement):
+    if statement.type == 'INSERT':
+        print('insert here')
+    elif statement.type == 'SELECT':
+        print('selecting')
+    else:
+        print('unknown type')
 
 main()
